8d841524440998857c402cc4fbccc9f1c4c6ca91
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-03-09 13:20:40 +0800

    更新申诉样式

--- templates/merchant/library/appeal_step.lbi.php
+++ templates/merchant/library/appeal_step.lbi.php
@@ -1,24 +1,24 @@
 <?php defined('IN_ECJIA') or exit('No permission resources.');?> 
-
 <div class="panel panel-body">
 	<div class="appeal-time-base m_b20">
 		<ul>
 			<li class="step-first">
 				<div class="{if $check_status eq '-1'}step-cur{else}step-done{/if}">
-					<div class="step-no">{if $check_status eq '-1'}1{/if}</div>
+					<div class="step-no">{if $check_status lt '1'}1{/if}</div>
 					<div class="m_t5">提交申诉</div>	
 				</div>
 			</li>
+			
 			<li>
-				<div class="{if $check_status eq '1'}step-cur{elseif $check_status eq '1'}step-done{/if}">
-					<div class="step-no">{if $check_status eq '1'}2{/if}</div>
+				<div class="{if $check_status eq '1'}step-cur{elseif $check_status gt '1'}step-done{/if}">
+					<div class="step-no">{if $check_status lt '2'}2{/if}</div>
 					<div class="m_t5">审核中</div>
 				</div>
 			</li>
 
 			<li class="step-last">
-				<div class="{if $step eq '3'}step-cur{/if}">
-					<div class="step-no">{if $step lt '5'}3{/if}</div>
+				<div class="{if $check_status eq '2'}step-cur{/if}">
+					<div class="step-no">{if $check_status lt '3'}3{/if}</div>
 					<div class="m_t5">申诉成功</div>
 				</div>
 			</li>
