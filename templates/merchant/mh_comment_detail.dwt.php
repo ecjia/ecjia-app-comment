60fe7e5aa15059da795df35ae3abfe282feeb2f0
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-03-13 18:54:56 +0800

    更新样式

--- templates/merchant/mh_comment_detail.dwt.php
+++ templates/merchant/mh_comment_detail.dwt.php
@@ -42,23 +42,62 @@
 						</div><br>
 						<a href='{url path="comment/mh_appeal/add_appeal" args="comment_id={$comment_id}"}'><button class="btn btn-info" type="button">申诉</button></a>
 		            </div>    
+				</div><br>
+				
+		        <div class="comment_all" id="accordionOne">
+		            <div class="panel panel-info">
+		                <div class="panel-heading">
+		                    <a data-toggle="collapse" data-parent="#accordionOne" href="#collapseOne" class="accordion-toggle">
+		                        <span class="glyphicon"></span>
+		                        <h4 class="panel-title">评论内容</h4>
+		                    </a>
+		                </div>
+		                <div id="collapseOne" class="panel-collapse collapse in">
+	                         <div class="comment-substance panel-body">
+	                              <div class="text-right">
+		                              <p>admin</p>
+		                              <p>2017-03-08 11:40:00</p>
+		                              <img src="https://cityo2o.ecjia.com/content/uploads/data/avatar_img/201701101718165302.png" />
+		                              <p>感谢您对本店的支持！我们会更加的努力，为您提供做优质的服务。</p>
+	                              </div>
+	                          </div>
+		                </div>
+		              </div>
+		        </div>
+		        
+				<div class="comment_top">
+					<div class="panel-body">
+						<a class="comment-thumb">
+							<img src="{$ecjia_main_static_url}img/ecjia_avatar.jpg" >
+						</a>
+						<div class="comment-thumb-details">
+							<h1>送钱</h1>
+							<p>2016-11-04 17：02:33<span>IP:10.10.10.41</span></p><br>
+						</div>
+						<div class="comment-goods">
+						  	<p>商品评分：</p>
+			                <p>收到手机已经过去三天了，我觉得无法使用，所以给差评</p>
+			                <img src="{$ecjia_main_static_url}img/ecjia_avatar.jpg">
+						</div><br>
+						<a href='{url path="comment/mh_appeal/add_appeal" args="comment_id={$comment_id}"}'><button class="btn btn-info" type="button">申诉</button></a>
+		            </div>    
 				</div> 
             </div>
         </section>
     </div>
     <div class="col-lg-4">
-        <div class="panel panel-default">
+        <div class="panel panel-body">
             <div class="appeal-goods">
-			  	<p>商品信息</p>
+			  	<h4>商品信息</h4>
 			  	<img src="{$ecjia_main_static_url}img/ecjia_avatar.jpg">
                 <p>收到手机已经过去三天了，我觉得无法使用，所以给差评</p>
                 <p>价格：</p>
                 <p>购买于：</p>
 			</div>
         </div>
-        <section class="panel">
+        <section class="panel panel-body">
           <div class="appeal-goods">
-			  	<p>其他评价</p>
+			  	<h4>其他评价</h4>
 			  	<p>用户名<span>查看</span></p><br>
                 <p>收到手机已经过去三天了，我觉得无法使用，所以给差评</p><br>
                 <p>收到手机已经过去三天了，我觉得无法使用，所以给差评</p>
