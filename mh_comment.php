071e11073954544afc51c03262143847bb9c8aa1
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-03-23 19:08:15 +0800

    test

--- mh_comment.php
+++ mh_comment.php
@@ -230,16 +230,15 @@ class mh_comment extends ecjia_merchant {
 			RC_DB::table('comment_reply')->insertGetId($data);
 			ecjia_merchant::admin_log('评论ID:'.$comment_id, 'reply', 'users_comment');
 		}
-// 		$comment_info = RC_DB::TABLE('comment')->where('comment_id', $comment_id)->select('id_value', 'status')->first();
-		$status = RC_DB::TABLE('comment')->where('comment_id', $comment_id)->pluck('status');
-		_dump($status,1);
-		if(intval($status) === 0){
-			_dump("test",1);
+		$id_value = RC_DB::TABLE('comment')->where('comment_id', $comment_id)->where('status', 0)->pluck('id_value');
+		_dump($id_value);
+		if(!empty($id_value)){
+			_dump($id_value,1);
 			$data = array(
 				'status' => 1
 			);
 			RC_DB::table('comment')->where('comment_id', $comment_id)->update($data);
-			RC_Api::api('comment', 'update_goods_comment', array('goods_id' => $comment_info['id_value']));
+			RC_Api::api('comment', 'update_goods_comment', array('goods_id' => $id_value));
 		}
 		
 	    return $this->showmessage('回复成功', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_SUCCESS, array('pjaxurl' => RC_Uri::url('comment/mh_comment/comment_detail',array('comment_id' => $comment_id))));
